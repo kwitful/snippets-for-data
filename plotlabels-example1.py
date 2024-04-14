b9c4af52f63477aa59ad3e92a97305546512624d
kwitful <119952091+kwitful@users.noreply.github.com>
2024-04-14 12:58:42 +0300

    Add files via upload

---
+++ plotlabels-example1.py
@@ -0,0 +1,19 @@
+# You can download the dataset from: https://www.kaggle.com/datasets/uciml/autompg-dataset
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Load the data frame, turn '?' entries into NaN
+auto_df = pd.read_csv("auto-mpg.csv", na_values='?')
+
+# Replace region numbers with actual region names
+auto_df.origin = auto_df.origin.replace({1:'USA', 2:'Europe', 3:'Asia'})
+
+# Scatterplot
+sns.scatterplot(x=auto_df.displacement, y= auto_df.mpg)
+plt.title("Displacement-MPG Relation") # Main title
+plt.xlabel("Engine displacement") # X-axis label
+plt.ylabel("Miles-per-gallon ratio") # Y-axis labe
+plt.show()
